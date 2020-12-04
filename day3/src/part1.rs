ab241b5a7b0414259d7c768dba13e63f0e5754af
Björn Söderqvist <cybear@gmail.com>
2020-12-04 22:58:17 +0100

    Cleanup day 3

--- day3/src/part1.rs
+++ day3/src/part1.rs
@@ -1,22 +1,21 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() {
-    let filename = "map.txt";
+pub fn get_n_trees() -> i32 {
+    let filename = "./day3/src/map.txt";
     let mut trees = 0;
     let mut x_pos = 0;
 
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    for (index, line) in reader.lines().enumerate() {
+    for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         let tile = line.chars().nth(x_pos).unwrap();
         if tile == '#' {
             trees = trees + 1;
         }
-        println!("{} line {} xpos {} tile {}", line, index, x_pos, tile);
         x_pos = (x_pos + 3) % line.len();
     }
-    println!("Trees: {}", trees);
+    trees
 }
--- day3/src/part2.rs
+++ day3/src/part2.rs
@@ -2,14 +2,12 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn count(right: usize, skip: bool) -> i32 {
-    let filename = "map.txt";
+    let filename = "./day3/src/map.txt";
     let mut trees = 0;
     let mut x_pos = 0;
 
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
-
-    println!("Counting trajectory x {} skipping? {}", right, skip);
 
     for (index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
@@ -22,18 +20,14 @@ fn count(right: usize, skip: bool) -> i32 {
             x_pos = (x_pos + right) % line.len();    
         }
     }
-
-    println!("Found {}", trees);
-    return trees;
+    trees
 }
 
-fn main() {
+pub fn get_n_trees() -> i32 {
     let path1 = count(1, false);
     let path2 = count(3, false);
     let path3 = count(5, false);
     let path4 = count(7, false);
     let path5 = count(1, true);
-    let multiplied = path1 * path2 * path3 * path4 * path5;
-    println!("Paths: {} {} {} {} {}", path1, path2, path3, path4, path5);
-    println!("Multiplied: {}", multiplied);
+    path1 * path2 * path3 * path4 * path5
 }
